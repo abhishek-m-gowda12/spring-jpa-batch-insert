08f6e48508b901cc97fce742582de1b4f43e657c
Abhishekgowda <abhishek@emproto.com>
2022-04-02 23:02:13 +0530

    initial commit

---
+++ src/main/java/com/abhishek/springjpabatchinsert/SpringJpaBatchInsertApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.springjpabatchinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBatchInsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaBatchInsertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhishek/springjpabatchinsert/SpringJpaBatchInsertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.springjpabatchinsert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBatchInsertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
